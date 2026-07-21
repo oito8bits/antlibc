ce823c271b99b189c4f090714515824dc4e395a4
oito8bits <williandossantosdeoliveira287@gmail.com>
2026-07-21 18:24:41 -0400

    Add strncmp() in string.h

--- include/string.h
+++ include/string.h
@@ -6,6 +6,7 @@
 size_t strlen(const char *);
 size_t memcmp(const void *buf1, const void *buf2, size_t count);
 size_t strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 void *strcpy(char *dest, const char *src);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *buff, int c, size_t n);
